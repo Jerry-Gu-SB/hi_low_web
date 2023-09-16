a3c4cb0911339ed23fa7a38b5aaf2583d2ee45bf
Jerry Gu <ncq9fn@virginia.edu>
2023-09-16 18:25:30 -0400

    button skeleton set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import QuizButton from "./components/QuizButton";
 
-function App() {
+const App: React.FC = () => {
+  const [isCorrectAnswer, setIsCorrectAnswer] = useState<boolean>(false);
+
+  const handleButtonClick = () => {
+    // Generate a random boolean value to determine correctness
+    const isCorrect = Math.random() < 0.5; // 50% chance of being correct
+
+    setIsCorrectAnswer(isCorrect);
+
+    // Handle any additional logic here, e.g., scoring, displaying feedback, etc.
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Simple React Typescript Tailwind Sample
+      </h1>
+      <div className="flex h-screen">
+        <QuizButton isCorrect={isCorrectAnswer} onClick={handleButtonClick} />
+        <QuizButton isCorrect={!isCorrectAnswer} onClick={handleButtonClick} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
